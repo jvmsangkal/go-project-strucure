b9d7fea4c56ce0368c1739876e0f6f8b0b3c7056
John Viscel Sangkal <johnviscelsangkal@gmail.com>
2015-10-29 21:27:24 +0800

    add CORS

--- config/config.go
+++ config/config.go
@@ -1,3 +1,16 @@
 package config
 
-const PORT = ":3000"
+import(
+    "github.com/rs/cors"
+)
+
+const (
+    HOST = "0.0.0.0"
+    PORT = ":3000"
+)
+
+var CORS = cors.New(cors.Options{
+    AllowedHeaders: []string{"Access-Token", "X-Requested-With", "Content-Type", "Accept"},
+    AllowedOrigins: []string{"*"},
+    AllowedMethods: []string{"GET", "POST", "PUT", "OPTIONS", "DELETE"},
+})
--- server.go
+++ server.go
@@ -12,7 +12,9 @@ func main() {
 
     router := lib.NewRouter()
 
-    log.Printf("Server Listening on localhost%s", config.PORT)
-    log.Fatal(http.ListenAndServe(config.PORT, router))
+    handler := config.CORS.Handler(router)
+
+    log.Printf("Server Listening on %s", config.HOST + config.PORT)
+    log.Fatal(http.ListenAndServe(config.HOST + config.PORT, handler))
 
 }
